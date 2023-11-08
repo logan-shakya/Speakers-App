a4987184b6276d550d4362c415105479ff2f9250
Nischal Shakya <shakyanischal2913@gmail.com>
2023-11-08 08:55:51 +0545

    Created custom hook, implemented favorite functionality

--- src/components/SpeakerList.js
+++ src/components/SpeakerList.js
@@ -1,42 +1,53 @@
 import Speaker from "./Speaker";
+import useRequestDelay, { REQUEST_STATUS } from "../hooks/useRequestDelay";
+import ReactPlaceholder from "react-placeholder";
 import { data } from "../../SpeakerData";
-import { useState } from "react";
 
 function SpeakerList({ showSessions }) {
 
-  const [ speakersData, setSpeakersData ] = useState(data);
+  const { 
+          data: speakersData, 
+          requestStatus, 
+          error, 
+          updateRecord 
+        } = useRequestDelay(2000, data);
 
-  function onFavoriteToggle(id) {
-    const speakersRecPrevious = speakersData.find( function (rec) {
-      return rec.id === id;
-    });
-    const speakerRecUpdated = {
-      ...speakersRecPrevious,
-      favorite:   !speakersRecPrevious.favorite
-    };
-    const speakersDataNew = speakersData.map(function (rec) {
-      return rec.id === id ? speakerRecUpdated : rec;
-    });
+  if(requestStatus === REQUEST_STATUS.FAILURE) {
+    return (
+      <div className="text-danger">
+        ERROR: <b>loading Speaker Data Failed {error}</b>
+      </div>
+    )
+  } 
 
-    setSpeakersData(speakersDataNew);
-  }
+  // if(isLoading === true) return <div>Loading...</div>
 
   return (
     <div className="container speakers-list">
-      <div className="row">
-        {speakersData.map(function (speaker) {
-          return (
-            <Speaker
-              key={speaker.id}
-              speaker={speaker}
-              showSessions={showSessions}
-              onFavoriteToggle = {() => {
-                onFavoriteToggle(speaker.id);
-              }}
-            />
-          );
-        })}
-      </div>
+      <ReactPlaceholder 
+        type="media"
+        rows={15}
+        className="speakerslist-placeholder"
+        ready={requestStatus === REQUEST_STATUS.SUCCESS}
+      >
+        <div className="row">
+          {speakersData.map(function (speaker) {
+            return (
+              <Speaker
+                key={speaker.id}
+                speaker={speaker}
+                showSessions={showSessions}
+                onFavoriteToggle={() => {
+                  updateRecord({
+                    ...speaker,
+                    favorite: !speaker.favorite,
+                  })
+                }}
+              />
+            );
+          })}
+        </div>
+      </ReactPlaceholder>
     </div>
   );
 }
